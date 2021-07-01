e33971e3471be13e1ac3afc843ece7734b4794da
Ernest <ernest-kies@wp.pl>
2021-07-01 13:10:11 +0200

    Add Query for countries

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import { Continent } from './Continents';
+
+interface Languages {
+  name: string;
+}
+
+interface Country {
+  name: string;
+  emoji: string;
+  language: Languages[];
+}
+
+interface CountriesData {
+  countries: Country[];
+}
+
+const GET_COUNTRIES_FROM_CONTINENT = gql`
+    query GetCountriesFromContinent($code: ID!) {
+      continent(code: $code) {
+        countries {
+          name
+          emoji
+          languages {
+            name
+          }
+        }
+      }
+    }
+  `
 
 export const Countries = () => {
+  const { code } = useParams<Omit<Continent, 'name'>>();
+  const { loading, error, data } = useQuery(GET_COUNTRIES_FROM_CONTINENT, {
+    variables: { code },
+  });
+
   return(
-    <div>asda</div>
+    <div>{code}</div>
   );
 };
