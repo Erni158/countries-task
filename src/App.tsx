71f203cd17517f2f60b6764f8e024c005bbde410
Ernest <ernest-kies@wp.pl>
2021-07-01 11:48:12 +0200

    Add basic Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { Continents } from './components/Continents';
 import './App.css';
 
 const App = () => {
   return (
       <div className="App">
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/continents">Continents</Link>
+              </li>
+            </ul>
+          </nav>
+            <Switch>
+              <Route path='/continents'>
+                <Continents />
+              </Route>
+            </Switch>
+        </Router>
       </div>
   );
 }
