c8316b0c035f63eb90116a04ddbb0d9741a26d76
Ernest <ernest-kies@wp.pl>
2021-07-01 13:46:40 +0200

    fix interface of countries, add styled-components layout

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
 import { Continent } from './Continents';
+import styled from 'styled-components';
 
 interface Languages {
   name: string;
@@ -10,34 +11,68 @@ interface Languages {
 interface Country {
   name: string;
   emoji: string;
-  language: Languages[];
+  languages: Languages[];
 }
 
 interface CountriesData {
-  countries: Country[];
+  continent: {
+    countries: Country[];
+  }
 }
 
 const GET_COUNTRIES_FROM_CONTINENT = gql`
-    query GetCountriesFromContinent($code: ID!) {
-      continent(code: $code) {
-        countries {
+  query GetCountriesFromContinent($code: ID!) {
+    continent(code: $code) {
+      countries {
+        name
+        emoji
+        languages {
           name
-          emoji
-          languages {
-            name
-          }
         }
       }
     }
-  `
+  }
+`
+
+const CountriesStyle = styled.div`
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr; 
+  grid-template-rows: 1fr 1fr 1fr; 
+  gap: 20px; 
+  grid-template-areas: 
+    ". . ."
+    ". . ."
+    ". . ."; 
+`
+
+const CountryItem = styled.div`
+  display: grid;
+  padding: 20px;
+  font-size: 20px;
+  border: 1px solid #000;
+  border-radius: 10px;
+`
 
 export const Countries = () => {
   const { code } = useParams<Omit<Continent, 'name'>>();
-  const { loading, error, data } = useQuery(GET_COUNTRIES_FROM_CONTINENT, {
+  const { loading, error, data } = useQuery<CountriesData, Omit<Continent, 'name'>>(GET_COUNTRIES_FROM_CONTINENT, {
     variables: { code },
   });
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: (</p>
+
+    console.log(data)
+
   return(
-    <div>{code}</div>
+    <CountriesStyle>
+      {data?.continent.countries.map(({name, emoji, languages}) => (
+        <CountryItem>
+          <span><strong>name:</strong> {name}</span>
+          <span><strong>emoji:</strong> {emoji}</span>
+          <span><strong>language:</strong> {languages[0]?.name ?? 'none'}</span>
+        </CountryItem>
+      ))}
+    </CountriesStyle>
   );
 };
