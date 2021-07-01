635006eaf85ed7c7c229a3c9332f05a43c3cfb8a
Ernest <ernest-kies@wp.pl>
2021-07-01 11:26:28 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
